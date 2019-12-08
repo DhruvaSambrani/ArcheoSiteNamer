8097c210bf62488ca5bd5e9d6e9c0b7e705c8193
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2019-12-08 22:28:42 +0530

    Update findaname.py

--- src/findaname.py
+++ src/findaname.py
@@ -1,6 +1,22 @@
 import random
-from itertools import combinations 
-def findabbrs(str, number=20):
+from itertools import combinations
+def findabbrs(str, max_number=20):
+    cleanStr = []
+    disp = []
+
+    # Remove non alpha numeric
+    for i in str[1:]:
+        if i.isalnum():
+            cleanStr.append(i.upper())
+
+    # Make a unique list of atmost max_number combinations of 2 chars
+    for i in list(set(combinations(cleanStr, 2)))[:max_number]:
+        t = list(i)
+        # Add first character
+        t.insert(0,str[0].upper())
+        disp.append("".join(t))
+    return disp
+'''
     l=len(str)
     disp=[]
     p=str.upper()
@@ -31,5 +47,7 @@ def findabbrs(str, number=20):
         for i in range(2):
             cd=cd+(chr(random.randint(ord("A"),ord("Z"))
         disp.append(cd)
-     
+
     return (disp)
+
+'''
