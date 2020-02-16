63f75ec8ed4bc52a0211d5df44e84f34f22df292
madhums511 <58359938+madhums511@users.noreply.github.com>
2020-02-16 21:07:50 +0530

    add wireframe files

---
+++ src/resc_db.py
@@ -0,0 +1,23 @@
+"""
+Database for Researcher.
+Table Name
+----------
+researcherTable
+
+Attributes
+----------
+name : STRING
+id : STRING :: PRIMARY KEY
+lab : STRING
+email_id : STRING
+"""
+
+
+"""
+fetch_by_id()
+fetch_by_lab()
+fetch_all()
+insert_all()
+delete_all()
+helpers..
+"""
---
+++ src/strc_map.py
@@ -0,0 +1,18 @@
+"""
+Site-Researcher Map
+Table Name
+----------
+strc_map
+
+Attributes
+----------
+site_id : STRING
+resc_id : STRING
+UNIQUE(site_id, resc_id)
+"""
+
+"""
+add_entry()
+fetch_by_resc()
+fetch_by_site()
+"""
