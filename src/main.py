5852b29537ec50beafcc4cf392fbd0e681f0f919
madhums511 <58359938+madhums511@users.noreply.github.com>
2020-02-21 02:40:02 +0530

    update

--- src/main.py
+++ src/main.py
@@ -3,20 +3,14 @@ from settings import SETTINGS, save
 import atexit
 from tabulate import tabulate
 import findaname
-from site_db import (fetch_all_site, fetch_by_id_site, HEADERS_SITE,
-                     delete_all_site, insert_site)
-from site_db import cleanup as dbcleanup_site
-from site_db import commit_changes as commitdb_site
-from resc_db import (fetch_by_rescID, fetch_all_resc, HEADERS_RESC,
-                     delete_all_resc, insert_resc)
-from site_db import cleanup as dbcleanup_resc
-from site_db import commit_changes as commitdb_resc
+from site_db import fetch_all, fetch_by_id, HEADERS, delete_all, insert
+from papers_db import cleanup as dbcleanup
+from dbhelper import commit_changes as commitdb
 
 
 @atexit.register
 def cleanup():
-    dbcleanup_site()
-    dbcleanup_resc()
+    dbcleanup()
     print("Thanks for using!")
 
 # ================= Functions ============== #
@@ -68,15 +62,15 @@ def add_site():
             '''Your preferred 3 character site CODE: ''')).upper()
         if len(abbr) != 3:
             print("Length of CODE must be 3")
-        elif len(fetch_by_id_site(major_zone, minor_zone, abbr)) == 1:
+        elif len(fetch_by_id(major_zone, minor_zone, abbr)) == 1:
             print("Site CODE already used.")
             print(
                 tabulate(
-                    fetch_by_id_site(
+                    fetch_by_id(
                         major_zone,
                         minor_zone,
                         abbr),
-                    HEADERS=HEADERS_SITE,
+                    HEADERS=HEADERS,
                     tablefmt="github"),
                 "\n")
         else:
@@ -95,7 +89,7 @@ def add_site():
         f"Enter 0 to reject, any key to accept: "
     ) != "0":
         try:
-            insert_site(
+            insert(
                 major_zone,
                 minor_zone,
                 lat,
@@ -105,39 +99,12 @@ def add_site():
                 description,
                 researcher,
                 oldcode)
-            commitdb_site()
+            commitdb()
             print("Site added to database")
         except Exception as e:
             print("Database error - ", e)
     else:
         print("Site add rejected")
-
-
-def add_resc():
-    researcher = str(input("Name of researcher: "))
-    rescID = str(input("Researcher ID: "))
-    lab = str(input("Laboratory: "))
-    email = str(input("E-mail ID: "))
-    if input(
-        f"Researcher - {researcher}\n"
-        f"ResearcherID - {rescID}\n"
-        f"Laboratory - {lab}\n"
-        f"EmailID - {email}\n"
-        f"Enter 0 to reject, any key to accept: "
-    ) != "0":
-        try:
-            insert_resc(
-                researcher,
-                rescID,
-                lab,
-                email)
-            commitdb_resc()
-            print("Researcher added to database")
-        except Exception as e:
-            print("Database error - ", e)
-
-    else:
-        print("Researcher add rejected")
 
 
 class InvalidInputError(Exception):
@@ -149,54 +116,33 @@ while True:
     try:
         INP = int(input(
             "1. Add a new Site\n"
-            "2. Add a new Researcher\n"
-            "3. List sites by CODE\n"
-            "4. List sites by Researcher ID\n"
-            "5. List all sites\n"
-            "6. List all researchers\n"
-            "7. Clear database\n"
-            "8. Settings\n"
+            "2. List sites by CODE\n"
+            "3. List all sites\n"
+            "4. Clear all sites\n"
+            "5. Settings\n"
             "0. Exit\n"
             "Your input: "))
         if INP == 1:
             add_site()
         elif INP == 2:
-            add_resc()
-        elif INP == 3:
             while True:
                 CODE = input("Enter CODE: ")
                 if len(CODE) == 5:
                     break
                 print("Invalid CODE")
-            print(tabulate(fetch_by_id_site(CODE[0],
-                                            CODE[1],
-                                            CODE[2:]),
-                           headers=HEADERS_SITE,
+            print(tabulate(fetch_by_id(CODE[0],
+                                       CODE[1],
+                                       CODE[2:]),
+                           headers=HEADERS,
                            tablefmt="github"),
                   "\n")
+        elif INP == 3:
+            print(tabulate(fetch_all(), headers=HEADERS, tablefmt="github"))
         elif INP == 4:
-            while True:
-                ID = input("Enter ID: ")
-            print(tabulate(fetch_by_rescID(ID),
-                           headers=HEADERS_RESC,
-                           tablefmt="github"),
-                  "\n")
+            if input("Are you sure?(y/N)").upper() == "Y":
+                delete_all()
+                print("Cleared")
         elif INP == 5:
-            print(tabulate(fetch_all_site(), headers=HEADERS_SITE,
-                           tablefmt="github"))
-        elif INP == 6:
-            print(tabulate(fetch_all_resc(), headers=HEADERS_RESC,
-                           tablefmt="github"))
-        elif INP == 7:
-            print("1. Clear all sites\n",
-                  "2. Clear all researchers\n")
-            if int(input("Your choice: ")) == 1:
-                delete_all_site()
-                print("Site database cleared")
-            elif int(input("Your choice: ")) == 2:
-                delete_all_resc()
-                print("Researcher database cleared")
-        elif INP == 8:
             print()
             change_settings()
         elif INP == 0:
--- src/site_db.py
+++ src/site_db.py
@@ -1,120 +1,182 @@
-import sqlite3
-from os.path import expanduser as homepath
+"""
+Database for Sites.
+Table Name
+----------
+siteTable
+Fields
+------
+Site: Class
+fetch_by_id()
+fetch_all()
+fetch_by_sql()
+"""
 
-HEADERS_SITE = [
-    "Site Name",
-    "Site Code",
-    "Site Description",
-    "Researcher",
+import pprsite_map
+from dbhelper import execute_sql, init_db
+init_db()
+
+
+class Site:
+    pass
+
+
+HEADERS = [
+    "Major Zone",
+    "Minor Zone",
+    "Latitude",
+    "Longitude",
+    "Code",
+    "Name",
+    "Description",
     "Old Code"]
-
 _ATTRMAP = {
     "majorZone": 0,
     "minorZone": 1,
     "latitude": 2,
     "longitude": 3,
-    "name": 4,
-    "abbr": 5,
+    "abbr": 4,
+    "name": 5,
     "description": 6,
-    "researcher": 7,
-    "oldcode": 8}
+    "oldcode": 7}
+
+
+class Site:
+    """ The Site object is a python representation
+of the entry held in the database.
+    Fields
+    ------
+        major_zone
+        minor_zone
+        latitude
+        longitude
+        name
+        abbr
+        description
+        oldcode
+    Functions
+    ---------
+        __init__(self, row: iterable)
+        __str__(self)
+        insert(self)
+        delete(self)
+"""
+    majorZone: str
+    minorZone: str
+    latitude: float
+    longitude: float
+    abbr: str
+    name: str
+    oldcode: str
+    description: str
+
+    def __init__(self, row):
+        """ Constructor
+Params
+------
+self: Site
+row: iterable
+    iterable indexed according _ATTRMAP
+"""
+        self.majorZone = row[_ATTRMAP["majorZone"]]
+        self.minorZone = row[_ATTRMAP["minorZone"]]
+        self.latitude = float(row[_ATTRMAP["latitude"]])
+        self.longitude = float(row[_ATTRMAP["longitude"]])
+        self.abbr = row[_ATTRMAP["abbr"]]
+        self.name = row[_ATTRMAP["name"]]
+        self.description = row[_ATTRMAP["description"]]
+        self.oldcode = row[_ATTRMAP["oldcode"]]
+
+    def __str__(self, desc_length=20):
+        temp = {}
+        temp["Name"] = self.name
+        temp["ID"] = self.majorZone +\
+            self.minorZone +\
+            self.abbr
+        temp["Longitude"] = self.longitude
+        temp["Latitude"] = self.latitude
+        temp["Description"] = self.description[:desc_length] +\
+            (self.description[desc_length:] and '...')
+        temp["Old Code"] = self.oldcode
+        return temp.__str__()
+
+    def fetch_papers(self):
+        return pprsite_map.fetch_paper_by_site(self.get_id())
+
+    def get_id(self):
+        return self.majorZone + self.minorZone + self.abbr
+
+    def insert(self):
+        """ Inserts self into db."""
+        execute_sql(
+            "INSERT INTO siteTable VALUES (?,?,?,?,?,?,?,?)",
+            (self.majorZone,
+             self.minorZone,
+             self.latitude,
+             self.longitude,
+             self.abbr,
+             self.name,
+             self.description,
+             self.oldcode))
+
+    def delete(self):
+        """Deletes self from db."""
+        execute_sql(
+            f'''
+            DELETE FROM "siteTable"
+            where majorZone = '{self.majorZone}' AND
+            minorZone = '{self.minorZone}' AND
+            abbr = '{self.abbr}' '''
+        )
 
 
 def _create_table():
-    _CURSOR.execute('''
+    execute_sql('''
     CREATE TABLE IF NOT EXISTS "siteTable" (
         "majorZone" TEXT NOT NULL,
         "minorZone" TEXT NOT NULL,
         "latitude" NUMBER NOT NULL,
         "longitude" NUMBER NOT NULL,
-        "name" TEXT NOT NULL,
         "abbr" TEXT NOT NULL,
+        "name" TEXT NOT NULL,
         "description" TEXT,
-        "researcher" TEXT NOT NULL,
         "oldcode" TEXT,
         PRIMARY KEY("majorZone", "minorZone", "abbr"),
         UNIQUE("majorZone", "minorZone", "abbr")
     )
     ''')
-    return _CURSOR
 
 
-def fetch_by_id_site(major, minor, abbr):
-    return [format_row(row) for row in _CURSOR.execute(
+def fetch_all():
+    """Return a list of all sites"""
+    return [Site(row) for row in execute_sql("SELECT * FROM siteTable")]
+
+
+def fetch_by_id(major, minor, abbr):
+    """Fetch the site by id
+Params
+------
+major: String
+    The major zone of the site
+minor: String
+    The minor zone of the site
+abbr: String
+    The abbr code of the site
+"""
+    return Site(execute_sql(
         f"SELECT * FROM siteTable WHERE majorZone = '{major}' AND "
-        f"minorZone = '{minor}' AND abbr = '{abbr}'")]
+        f"minorZone = '{minor}' AND abbr = '{abbr}'").fetchone())
 
 
-def fetch_by_researcher(researcher):
-    return [format_row(row) for row in _CURSOR.execute(
-        f"SELECT * FROM siteTable WHERE researcher = '{researcher}'")]
+def fetch_by_sql(clause, param):
+    """Fetch sites by query
+clause: String
+    Contains the clause for the query. SELECT will be added automatically.
+    eg: "WHERE name='?'"
+params: Tuple = ()
+    Arguments to add to clause
+"""
+    query = '''SELECT * from siteTable ''' + clause
+    return [Site(row) for row in execute_sql(query, param)]
 
 
-def fetch_all_site():
-    return [format_row(row)
-            for row in _CURSOR.execute("SELECT * FROM siteTable")]
-
-
-def format_row(row, desc_length=20):
-    temp = []
-    temp.append(row[_ATTRMAP["name"]])
-    temp.append(row[_ATTRMAP["majorZone"]] +
-                row[_ATTRMAP["minorZone"]] +
-                row[_ATTRMAP["abbr"]])
-    temp.append(row[_ATTRMAP["description"]][:desc_length]
-                + (row[_ATTRMAP["description"]][desc_length:] and '...'))
-    temp.append(row[_ATTRMAP["researcher"]])
-    temp.append(row[_ATTRMAP["oldcode"]])
-    return temp
-
-
-def execute_sql(query):
-    _CURSOR.execute(query)
-
-
-def insert_site(major_zone, minor_zone, latitude, longitude,
-                name, abbr, description, researcher, oldcode):
-    """
-    Inserts values into db.
-    Parameters
-    ----------
-    major_zone
-    minor_zone
-    latitude
-    longitude
-    name
-    abbr
-    description
-    researcher
-    oldcode
-    """
-    _CURSOR.execute(
-        "INSERT INTO siteTable VALUES (?,?,?,?,?,?,?,?,?)",
-        (major_zone,
-         minor_zone,
-         latitude,
-         longitude,
-         name,
-         abbr,
-         description,
-         researcher,
-         oldcode))
-
-
-def delete_all_site():
-    _CURSOR.execute('''DELETE FROM "mainTable"''')
-
-
-def commit_changes():
-    _CONN.commit()
-
-
-def cleanup():
-    _CONN.commit()
-    _CONN.close()
-
-
-# =========== INIT ============== #
-_CONN = sqlite3.connect(homepath('~/.ArcheoSiteNamer/database.db'))
-_CURSOR = _CONN.cursor()
 _create_table()
