abde6f5cc04db673e92f5c3b4e0055178b4d9d07
madhums511 <58359938+madhums511@users.noreply.github.com>
2020-02-22 12:59:39 +0530

    Update resc_db.py

--- src/resc_db.py
+++ src/resc_db.py
@@ -1,12 +1,17 @@
 
-import sqlite3
-from os.path import expanduser as homepath
-HEADERS_RESC = [
+from dbhelper import execute_sql, init_db
+init_db()
+
+
+class Researcher:
+    pass
+
+
+HEADERS = [
     "Researcher",
     "Researcher ID",
-    "Laboratory ",
-    "E-mail ID "]
-
+    "Laboratory",
+    "E-mail ID"]
 _ATTRMAP = {
     "researcher": 0,
     "rescID": 1,
@@ -14,75 +19,105 @@ _ATTRMAP = {
     "email": 3}
 
 
+class Researcher:
+    """ The Researcher object is a representation of a database with the
+        following attributes:
+    Attributes
+    ------
+    researcher
+    researcher ID
+    Laboratory
+    Email ID
+
+    Functions
+    ---------
+        __init__(self, row: iterable)
+        __str__(self)
+        insert(self)
+        delete(self)
+"""
+    researcher: str
+    rescID: str
+    lab: str
+    email: str
+
+    def __init__(self, row):
+        """ Constructor
+Parameters
+------
+self: Researcher
+row: iterable
+    iterable indexed according _ATTRMAP
+"""
+        self.researcher = row[_ATTRMAP["researcher"]]
+        self.rescID = row[_ATTRMAP["rescID"]]
+        self.lab = row[_ATTRMAP["lab"]]
+        self.email = row[_ATTRMAP["email"]]
+
+    def __str__(self):
+        temp = {}
+        temp["Researcher"] = self.researcher
+        temp["rescID"] = self.rescID
+        temp["lab"] = self.lab
+        temp["EmailID"] = self.email
+        return temp.__str__()
+
+    def insert(self):
+        """ Inserts self into database"""
+        execute_sql(
+            "INSERT INTO rescTable VALUES (?,?,?,?)",
+            (self.researcher,
+             self.rescID,
+             self.lab,
+             self.email))
+
+    def delete(self):
+        """Deletes self from database."""
+        execute_sql(
+            f'''
+            DELETE FROM "rescTable"
+            where rescID='{self.rescID}' ''')
+
+
 def _create_table():
-    _CURSOR.execute('''
-    CREATE TABLE IF NOT EXISTS "researcherTable" (
+    execute_sql('''
+    CREATE TABLE IF NOT EXISTS "rescTable" (
         "researcher" TEXT NOT NULL,
-        "rescID" TEXT NOT NULL,
-        "lab" TEXT NOT NULL,
-        "email" TEXT NOT NULL
+        "rescID" TEXT NOT NULL
+        "lab" TEXT
+        "email" TEXT
         PRIMARY KEY("rescID"),
         UNIQUE("rescID")
     )
     ''')
-    return _CURSOR
 
 
-def fetch_by_rescID(rescID):
-    return [format_row(row) for row in _CURSOR.execute(
-        f"SELECT * FROM researcherTable WHERE rescID='{rescID}'")]
+def fetch_all():
+    """Return a list of all researchers in the database"""
+    return [Researcher(row) for row in execute_sql("SELECT * FROM rescTable")]
 
 
-def fetch_all_resc():
-    return [format_row(row)
-            for row in _CURSOR.execute("SELECT * FROM researcherTable")]
+def fetch_by_id(rescID):
+    """Fetch the researcher by id
+Parameters
+------
+major: rescID
+    The unique researcher ID for every researcher
+"""
+    return Researcher(execute_sql(
+        f"SELECT * FROM rescTable WHERE rescID = '{rescID}'").fetchone())
 
 
-def format_row(row):
-    temp = []
-    temp.append(row[_ATTRMAP["researcher"]])
-    temp.append(row[_ATTRMAP["rescID"]])
-    temp.append(row[_ATTRMAP["lab"]])
-    temp.append(row[_ATTRMAP["email"]])
-    return temp
+def fetch_by_sql(clause, param):
+    """Fetch researcher details by query
+clause: String
+    Contains the clause for the query. SELECT will be added automatically.
+    eg: "WHERE name='?'"
+params: Tuple = ()
+    Arguments to add to clause
+"""
+    query = '''SELECT * from rescTable ''' + clause
+    return [Researcher(row) for row in execute_sql(query, param)]
 
 
-def execute_sql(query):
-    _CURSOR.execute(query)
-
-
-def insert(researcher, rescID, lab, email):
-    """
-    Inserts values into databas.
-    Parameters
-    ----------
-    researcher
-    researcher ID
-    Laboratory
-    E-mail ID
-    """
-    _CURSOR.execute(
-        "INSERT INTO researcherTable VALUES (?,?,?,?)",
-        (researcher,
-         rescID,
-         lab,
-         email))
-
-
-def delete_all_resc():
-    _CURSOR.execute('''DELETE FROM "mainTable"''')
-
-
-def commit_changes():
-    _CONN.commit()
-
-
-def cleanup():
-    _CONN.commit()
-    _CONN.close()
-
-
-# =========== INIT ============== #
-_CONN = sqlite3.connect(homepath('~/.ArcheoSiteNamer/database.db'))
-_CURSOR = _CONN.cursor()
 _create_table()
